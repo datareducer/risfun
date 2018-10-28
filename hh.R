2eaa07641eb8dbe3ed25b6938f69f8248ccadcb2
Kirill <kamikhailov@gmail.com>
2018-10-28 13:28:40 +0300

    Анализ рынка труда по данным HeadHunter

--- hh.R
+++ hh.R
@@ -1,14 +1,13 @@
-# Анализ рынка труда 1С
+# Анализ рынка труда по данным HeadHunter
 
 library('httr')
 library('jsonlite')
 library('ggplot2')
-
-token <- 'CHANGE_ME'
+library('R.cache')
 
 experience <- c("noExperience", "between1And3", "between3And6", "moreThan6")
 
-search_1c <- '1c%20NOT%20Bitrix';
+search_1c <- '1c+NOT+Bitrix';
 search_java <- 'java';
 search_javascript <- 'javascript';
 search_php <- 'php';
@@ -19,31 +18,41 @@ search_go <- 'go';
 
 getdata <- function(url) {
     out <- tryCatch({
-      resp <- GET(url, add_headers(Authorization = sprintf("Bearer %s", token)))
-      fromJSON(content(resp, as="text") )
+      key <- as.list(url)
+      resp <- loadCache(key)
+      if (is.null(resp)) {
+        Sys.sleep(0.5)
+        resp_txt <- GET(url)
+        resp <- fromJSON(content(resp_txt, as="text"), flatten=TRUE)
+        saveCache(resp, key)
+      }
+      resp
     },
     error=function(cond) {
-      message(cond)
+      print(cond)
       return(NULL)
-    }
-  )    
-  return(out)
+    },
+    warning=function(cond) {
+      print(cond)
+      return(NULL)
+    })
+    return(out)
 }
 
 vacancies <- function(area, search) {
   result <- NULL
   
   for (exp in experience) {
-    i <- 0
+    page <- 0
     pages <- -1
     
-    while (i != pages) {
-      url <- sprintf("https://api.hh.ru/vacancies?specialization=1&area=%d&experience=%s&page=%d&per_page=100&search_field=name&text='%s'",
-                     area, exp, i, search)
+    while (page != pages) {
+      url <- sprintf("https://api.hh.ru/vacancies?specialization=1&area=%d&experience=%s&page=%d&per_page=100&search_field=name&text=%s",
+                     area, exp, page, search)
       
       df <- getdata(url)
       if (is.null(df)) {
-        break
+        stop("Сетевая ошибка")
       }
       
       pages <- df$pages
@@ -51,10 +60,8 @@ vacancies <- function(area, search) {
         break
       }
       
-      items <- df$items[,c('id','area','employer','name','salary')]
-      items <- cbind(items[,c('id','name')], 
-                       area_name=items$area$name, employer_name=items$employer$name, salary_currency=items$salary$currency, 
-                       salary_gross=items$salary$gross, salary_from=items$salary$from, salary_to=items$salary$to, Опыт=exp)
+     items <- df$items[,c('id','area.name','employer.name','name','salary.currency','salary.gross','salary.from','salary.to')]
+     items$experience <- factor(exp)
         
      if (is.null(result)) {
         result <- items 
@@ -62,7 +69,7 @@ vacancies <- function(area, search) {
         result <- rbind(result, items)
      }
 
-      i <- i+1
+      page <- page+1
     }
   }
   
@@ -121,83 +128,23 @@ combined <- rbind(moscow_1c, spetersburg_1c, moscow_java, spetersburg_java, mosc
                   spetersburg_php, moscow_csharp, spetersburg_csharp, moscow_python, spetersburg_python, moscow_cplusplus,
                   spetersburg_cplusplus, moscow_go, spetersburg_go)
 
-combined$salary <- with(combined, ifelse(is.na(combined$salary_to), combined$salary_from, combined$salary_to))
+combined$salary <- with(combined, ifelse(is.na(combined$salary.to), combined$salary.from, combined$salary.to))
 
 # Удалим явные выбросы и оклады в иностранной валюте (их немного)
-combined <- subset(combined, salary <= 350000 & salary_currency == "RUR")
+combined <- subset(combined, salary <= 350000 & salary.currency == "RUR")
 
 # Учитываем НДФЛ
-combined$salary <- with(combined, ifelse(combined$salary_gross, combined$salary*0.87, combined$salary))
+combined$salary <- with(combined, ifelse(combined$salary.gross, combined$salary*0.87, combined$salary))
 
 # Отключаем Научную нотацию чисел на диаграммах
 options(scipen=999)
 
-qplot(area_name, salary, data=subset(combined, lang=="1C"), geom=c("jitter", "boxplot"), alpha=I(0.8), colour=Опыт, 
-      main="Диаграмма предложений на рынке труда 1С (Октябрь 2018)", xlab="", ylab="Заработная плата ('net')", varwidth = TRUE)    
+p <- qplot(area.name, salary, data=subset(combined, lang=="1C"), geom=c("jitter", "boxplot"), alpha=I(0.8), colour=experience, 
+      main="Диаграмма предложений на рынке труда 1С", xlab="", ylab='Заработная плата ("на руки")', varwidth = TRUE)
+p
 
-qplot(lang, salary, data=combined, geom="boxplot", alpha=I(1), main="Сравнение предложений на рынке труда для разработчиков (Октябрь 2018)", 
-      xlab="", ylab="Заработная плата ('net')", varwidth = TRUE, colour=area_name)
+#p + geom_hline(aes(yintercept=160000)) + geom_text(aes(0,160000,label = 'Тест', vjust = -0.5, hjust=-0.5), inherit.aes=FALSE)
+
+qplot(lang, salary, data=combined, geom="boxplot", alpha=I(1), main="Сравнение предложений на рынке труда для разработчиков", 
+      xlab="", ylab="Заработная плата ('net')", varwidth = TRUE, colour=area.name)
       
-################## Анализ текста вакансий ##################
-
-# Получаем полные описания вакансий (в поиске выдаются сокращенные)
-vacancies_id <- rbind(moscow_1c, spetersburg_1c)$id
-
-descriptions <- data.frame(id=character(), desc=character())
-i <- 0
-for (vid in vacancies_id) {
-  url <- sprintf("https://api.hh.ru/vacancies/%s", vid)
-  
-  df <- getdata(url)
-  if (is.null(df)) {
-    next
-  }
-  
-  descriptions <- rbind(descriptions, data.frame(id=vid, desc=gsub("<.*?>", "", df$description), stringsAsFactors=FALSE))
-}
-
-with(descriptions, {
-  
-  has_kd <- grepl("(1C:|1С:|\\s)КД\\W|Конвертация", desc, ignore.case = TRUE)
-  has_erp <- grepl("(1С:|\\s)ERP\\W|Управление предприятием", desc, ignore.case = TRUE)
-  has_uh <- grepl("(1C:|1С:|\\s)УХ\\W|Управление холдингом", desc, ignore.case = TRUE)
-
-  has_bgu <- grepl("(1C:|1С:|\\s)БГУ\\W|Бухгалтерия государственного учреждения", desc, ignore.case = TRUE)
-  has_bp <- grepl("(1C:|1С:|\\s)БП\\W|Бухгалтерия предприятия|Бухгалтерия", desc, ignore.case = TRUE)
-  has_dok <- grepl("Документооборот", desc, ignore.case = TRUE)
-  
-  has_zikgu <- grepl("(1C:|1С:|\\s)ЗИКГУ\\W|Зарплата и кадры государственного учреждения", desc, ignore.case = TRUE)
-  has_zup <- grepl("(1C:|1С:|\\s)ЗУП\\W|Зарплата и Управление Персоналом|(1C|1С):?\\s?Зарплата", desc, ignore.case = TRUE)
-  has_ka <- grepl("(1C:|1С:|\\s)КА\\W|Комплексная автоматизация", desc, ignore.case = TRUE)
-  
-  has_unf <- grepl("(1C:|1С:|\\s)УНФ\\W|Управление нашей фирмой", desc, ignore.case = TRUE)
-  has_rozn <- grepl("Розница", desc, ignore.case = TRUE)
-  has_upp <- grepl("(1C:|1С:|\\s)УПП\\W|Управление производственным предприятием", desc, ignore.case = TRUE)
-  has_ut <- grepl("(1C:|1С:|\\s)УТ\\W|Управление торговлей|Торговля", desc, ignore.case = TRUE)
-  
-  conf_count <<- data.frame(conf=character(), count=integer())
-  
-  conf_count <<- rbind(conf_count, data.frame(conf="1С:Конвертация данных", count=nrow(descriptions[has_kd==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="1C:ERP Управление предприятием", count=nrow(descriptions[has_erp==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="1С:Управление холдингом", count=nrow(descriptions[has_uh==TRUE,])))
-  
-  conf_count <<- rbind(conf_count, data.frame(conf="Бухгалтерия государственного учреждения", count=nrow(descriptions[has_bgu==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="Бухгалтерия предприятия", count=nrow(descriptions[has_bp==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="Документооборот", count=nrow(descriptions[has_dok==TRUE,])))
-  
-  conf_count <<- rbind(conf_count, data.frame(conf="Зарплата и кадры государственного учреждения", count=nrow(descriptions[has_zikgu==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="Зарплата и Управление Персоналом", count=nrow(descriptions[has_zup==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="Комплексная автоматизация", count=nrow(descriptions[has_ka==TRUE,])))
-  
-  conf_count <<- rbind(conf_count, data.frame(conf="Управление нашей фирмой", count=nrow(descriptions[has_unf==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="Розница", count=nrow(descriptions[has_rozn==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="Управление производственным предприятием", count=nrow(descriptions[has_upp==TRUE,])))
-  conf_count <<- rbind(conf_count, data.frame(conf="Управление торговлей", count=nrow(descriptions[has_ut==TRUE,])))
-  
-})
-
-ggplot(conf_count, aes(x = reorder(conf, count), y = count)) + geom_bar(stat='identity', position = 'dodge', fill = '#ffdd03', width=0.8) +
-  coord_flip() + ylab('Количество вакансий') + xlab('') + ggtitle('Конфигурации фирмы "1С" по упоминаниям в вакансиях (Октябрь 2018)')
-
-
-
