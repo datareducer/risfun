7661e7e4820777cb399279590fdfc98b1be7dfdc
Kirill <kamikhailov@gmail.com>
2018-11-16 23:38:21 +0300

    Анализ рынка труда по данным HeadHunter

--- hh.R
+++ hh.R
@@ -1,150 +1,335 @@
 # Анализ рынка труда по данным HeadHunter
+# @author Kirill Mikhaylov
 
-library('httr')
-library('jsonlite')
-library('ggplot2')
-library('R.cache')
+library(httr)
+library(jsonlite)
+library(ggplot2)
+library(R.cache)
+library(Cairo)
 
-experience <- c("noExperience", "between1And3", "between3And6", "moreThan6")
+Sys.setenv(TZ = "Europe/Moscow")
+resourceHostDir <- '/mnt/host/${resourceName}'
+dir.create(resourceHostDir)
+logFile <- paste(resourceHostDir, paste('log_', Sys.Date(), '.txt', sep=''), sep='/')
 
-search_1c <- '1c+NOT+Bitrix';
-search_java <- 'java';
-search_javascript <- 'javascript';
-search_php <- 'php';
-search_csharp <- 'c#';
-search_python <- 'python';
-search_cplusplus <- 'c++';
-search_go <- 'go';
+# R.cache не позволяет явно задать время жизни кэша. 
+# Обходим это, создавая новый каталог для кэширования каждый день. Таким образом, время жизни кэша будет равняться суткам.
+cacheDir <- paste('/tmp/Rcache/${resourceName}', Sys.Date(), sep='/')
+dir.create(cacheDir, recursive=TRUE)
+setCacheRootPath(path=cacheDir)
 
-getdata <- function(url) {
+log <- function(message) {
+    write(paste(Sys.time(), message, sep=" "), file=logFile, append=TRUE)
+}
+
+getData <- function(url) {
     out <- tryCatch({
       key <- as.list(url)
-      resp <- loadCache(key)
+      resp <- loadCache(key, onError=c("quiet"))
       if (is.null(resp)) {
+        log(url)
         Sys.sleep(0.5)
-        resp_txt <- GET(url)
+        resp_txt <- GET(url, user_agent("DataReducer/dev (admin@datareducer.ru)"))
         resp <- fromJSON(content(resp_txt, as="text"), flatten=TRUE)
         saveCache(resp, key)
+      } else {
+        log(paste(url, "(Получено из кэша)", sep=" "))
       }
       resp
     },
     error=function(cond) {
-      print(cond)
-      return(NULL)
+        log(cond)
+        stop(cond)
     },
     warning=function(cond) {
-      print(cond)
-      return(NULL)
+        log(cond)
+        stop(cond)
     })
     return(out)
 }
 
-vacancies <- function(area, search) {
-  result <- NULL
-  
-  for (exp in experience) {
-    page <- 0
-    pages <- -1
-    
-    while (page != pages) {
-      url <- sprintf("https://api.hh.ru/vacancies?specialization=1&area=%d&experience=%s&page=%d&per_page=100&search_field=name&text=%s",
-                     area, exp, page, search)
-      
-      df <- getdata(url)
-      if (is.null(df)) {
-        stop("Сетевая ошибка")
-      }
-      
-      pages <- df$pages
-      if(pages == 0 || length(df$items) == 0 || class(df$items$salary) == "logical") {
-        break
-      }
-      
-     items <- df$items[,c('id','area.name','employer.name','name','salary.currency','salary.gross','salary.from','salary.to')]
-     items$experience <- factor(exp)
-        
-     if (is.null(result)) {
-        result <- items 
-     } else {
-        result <- rbind(result, items)
-     }
+getVacancies <- function(exp, search) {
+    result <- NULL
 
-      page <- page+1
+    p <- 0
+    pages <- -1
+    while (p != pages) {
+        url <- "https://api.hh.ru/vacancies";
+        
+        params_list <- list()
+        
+        if (!is.na(specialization_list)) {
+            names(specialization_list) <- rep(c("specialization"), length(specialization_list))
+            params_list <- append(params_list, specialization_list)
+        }
+        
+        if (!is.na(area_list)) {
+            names(area_list) <- rep(c("area"), length(area_list))
+            params_list <- append(params_list, area_list)
+        }
+        
+        if (!is.na(industry_list)) {
+            names(industry_list) <- rep(c("industry"), length(industry_list))
+            params_list <- append(params_list, industry_list)
+        }
+        
+        if (!is.na(schedule_list)) {
+            names(schedule_list) <- rep(c("schedule"), length(schedule_list))
+            params_list <- append(params_list, schedule_list)
+        }
+        
+        # Параметры, имеющие несколько значений для одних и тех же имён, надо установливать первыми
+        url <- modify_url(url, query = params_list)
+        
+        url <- modify_url(url, query = list(page=p, per_page=100))
+        url <- modify_url(url, query = list(only_with_salary="true", no_magic="false", search_field="name"))
+        url <- modify_url(url, query = list(text=search))
+        
+        if (!is.null(exp)) {
+            url <- modify_url(url, query = list(experience=exp))
+        }
+      
+        df <- getData(url)
+      
+        pages <- df$pages
+        if(pages == 0 || length(df$items) == 0 || class(df$items$salary) == "logical") {
+            break
+        }
+      
+        items <- df$items[,c('id','alternate_url','area.name','employer.name','name','salary.currency','salary.gross','salary.from','salary.to')]
+        
+        items$search <- factor(search)
+        
+        if (!is.null(exp)) {
+            items$experience <- factor(exp)
+        }
+        
+        if (is.null(result)) {
+            result <- items 
+        } else {
+            result <- rbind(result, items)
+        }
+        p <- p+1
     }
-  }
   
   return(result)
 }
 
-moscow_1c <- vacancies(1, search_1c) 
-spetersburg_1c <- vacancies(2, search_1c)
+log("========== Начало выполнения ==========")
 
-moscow_1c$lang <- "1C"
-spetersburg_1c$lang <- "1C"
+# Проверка и обработка параметров
 
-moscow_java <- vacancies(1, search_java)
-spetersburg_java <- vacancies(2, search_java)
+# Строка поиска
+search_str <- trimws('${search}')
 
-moscow_java$lang <- "Java"
-spetersburg_java$lang <- "Java"
+if (search_str == '' || search_str == 'NA') {
+    stop("Не установлено значение строки поиска")
+}
+search_list <- as.list(strsplit(search_str, ",")[[1]]) 
 
-moscow_javascript <- vacancies(1, search_javascript)
-spetersburg_javascript <- vacancies(2, search_javascript)
+# График работы
+schedule_str <- '${schedule}'
 
-moscow_javascript$lang <- "JavaScript"
-spetersburg_javascript$lang <- "JavaScript"
+if (schedule_str == 'NA') {
+    schedule_list <- NA
+} else {
+    if (!grepl("^(fullDay|shift|flexible|remote|flyInFlyOut)(,(fullDay|shift|flexible|remote|flyInFlyOut))*$", schedule_str)) {
+        stop(paste("Недопустимое значение параметра графика работы:", schedule_str, sep=" "))        
+    } else {
+        schedule_list <- as.list(strsplit(schedule_str, ",")[[1]]) 
+    }
+}
 
-moscow_php <- vacancies(1, search_php)
-spetersburg_php <- vacancies(2, search_php)
+# Регион
+area_str <- '${area}'
 
-moscow_php$lang <- "PHP"
-spetersburg_php$lang <- "PHP"
+if (area_str == 'NA') {
+    area_list <- NA
+} else {
+    if (!grepl("^\\d+(,\\d+)*$", area_str)) {
+        stop(paste("Недопустимое значение параметра региона:", area_str, sep=" "));
+    } else {
+        area_list <- as.list(strsplit(area_str, ",")[[1]])    
+    }
+}
 
-moscow_csharp <- vacancies(1, search_csharp)
-spetersburg_csharp <- vacancies(2, search_csharp)
+# Профобласть или специализация
+specialization_str <- '${specialization}'
 
-moscow_csharp$lang <- "C#"
-spetersburg_csharp$lang <- "C#"
+if (specialization_str == 'NA') {
+    specialization_list <- NA
+} else {
+    if (!grepl("^\\d+(\\.\\d+)?(,\\d+(\\.\\d+)?)*$", specialization_str)) {
+        stop(paste("Недопустимое значение параметра специализации:", specialization_str, sep=" "));
+    } else {
+        specialization_list <- as.list(strsplit(specialization_str, ",")[[1]])    
+    }   
+}
 
-moscow_python <- vacancies(1, search_python)
-spetersburg_python <- vacancies(2, search_python)
+# Индустрия компании
+industry_str <- '${industry}'
 
-moscow_python$lang <- "Python"
-spetersburg_python$lang <- "Python"
+if (industry_str == 'NA') {
+    industry_list <- NA
+} else {
+    if (!grepl("^\\d+(\\.\\d+)?(,\\d+(\\.\\d+)?)*$", industry_str)) {
+        stop(paste("Недопустимое значение параметра индустрии компании:", industry_str, sep=" "));
+    } else {
+        industry_list <- as.list(strsplit(industry_str, ",")[[1]])    
+    }   
+}
 
-moscow_cplusplus <- vacancies(1, search_cplusplus)
-spetersburg_cplusplus <- vacancies(2, search_cplusplus)
+# Группировка по опыту
+expcolour <- as.logical('${expcolour}')
+if (is.na(expcolour)) {
+    expcolour <- FALSE
+}
 
-moscow_cplusplus$lang <- "C++"
-spetersburg_cplusplus$lang <- "C++"
+# Вывод точек вакансий (диаграмма рассеяния)
+jitter <- as.logical('${jitter}')
+if (is.na(jitter)) {
+    jitter <- FALSE
+}
 
-moscow_go <- vacancies(1, search_go)
-spetersburg_go <- vacancies(2, search_go)
+# Параметры выбросов
+outliermin_str <- '${outliermin}'
+if (outliermin_str == "NA") {
+    outliermin <- NA
+} else {
+    outliermin <- as.numeric(outliermin_str)
+    if (is.na(outliermin)) {
+        stop(paste("Недопустимое значение параметра нижней границы выбросов:", outliermin_str, sep=" "))
+    }
+}
+outliermax_str <- '${outliermax}'
+if (outliermax_str == "NA") {
+    outliermax <- NA
+} else {
+    outliermax <- as.numeric(outliermax_str)
+    if (is.na(outliermax)) {
+        stop(paste("Недопустимое значение параметра нижней границы выбросов:", outliermax_str, sep=" "))
+    }
+}
 
-moscow_go$lang <- "Go"
-spetersburg_go$lang <- "Go"
+# Заголовок диаграммы
+title <- trimws('${title}')
 
-combined <- rbind(moscow_1c, spetersburg_1c, moscow_java, spetersburg_java, moscow_javascript, spetersburg_javascript, moscow_php, 
-                  spetersburg_php, moscow_csharp, spetersburg_csharp, moscow_python, spetersburg_python, moscow_cplusplus,
-                  spetersburg_cplusplus, moscow_go, spetersburg_go)
+# Размеры диаграммы
+plotwidth_str <- '${plotwidth}'
+plotwidth <- as.numeric(plotwidth_str)
+if (is.na(plotwidth)) {
+    stop(paste("Недопустимое значение параметра ширины диаграммы:", plotwidth_str, sep=" "))
+}
+plotheight_str <- '${plotheight}'
+plotheight <- as.numeric(plotheight_str)
+if (is.na(plotheight)) {
+    stop(paste("Недопустимое значение параметра высоты диаграммы:", plotheight_str, sep=" "))
+}
 
-combined$salary <- with(combined, ifelse(is.na(combined$salary.to), combined$salary.from, combined$salary.to))
+# Ширина ящиков зависит от количества вакансий в соответствующей группе
+boxwidth <- as.logical('${boxwidth}')
+if (is.na(jitter)) {
+    boxwidth <- FALSE
+}
 
-# Удалим явные выбросы и оклады в иностранной валюте (их немного)
-combined <- subset(combined, salary <= 350000 & salary.currency == "RUR")
+vacancies <- NULL
+
+{ # XXX Цикл, не вложенный в фигурные скобки, приводит к ошибке RServe
+for (search in search_list) {
+    if (expcolour) {
+        experience <- c("noExperience", "between1And3", "between3And6", "moreThan6")    
+        for (exp in experience) {
+            result <- getVacancies(exp, search) 
+            if (is.null(vacancies)) {
+                vacancies <- result
+            } else {
+                vacancies <- rbind(result, vacancies)
+            }
+        } 
+    } else {
+        result <- getVacancies(NULL, search) 
+        if (is.null(vacancies)) {
+            vacancies <- result
+        } else {
+            vacancies <- rbind(result, vacancies)
+        }
+    }
+}
+} # XXX
+
+if (length(vacancies) == 0) {
+    stop("Отсутствуют элементы для отображения")
+}
+
+vacancies$salary <- with(vacancies, ifelse(is.na(vacancies$salary.to), vacancies$salary.from, vacancies$salary.to))
+
+# Удаляем оклады в иностранной валюте
+vacancies <- subset(vacancies, salary.currency == "RUR")
+
+# Удаляем выбросы
+if (!is.na(outliermin)) {
+    vacancies <- subset(vacancies, salary >= outliermin)
+}
+
+if (!is.na(outliermax)) {
+    vacancies <- subset(vacancies, salary <= outliermax)
+}
 
 # Учитываем НДФЛ
-combined$salary <- with(combined, ifelse(combined$salary.gross, combined$salary*0.87, combined$salary))
+vacancies$salary <- with(vacancies, ifelse(vacancies$salary.gross, vacancies$salary*0.87, vacancies$salary))
 
 # Отключаем Научную нотацию чисел на диаграммах
 options(scipen=999)
 
-p <- qplot(area.name, salary, data=subset(combined, lang=="1C"), geom=c("jitter", "boxplot"), alpha=I(0.8), colour=experience, 
-      main="Диаграмма предложений на рынке труда 1С", xlab="", ylab='Заработная плата ("на руки")', varwidth = TRUE)
-p
+CairoFonts(regular='DejaVu Serif:style=Regular')
 
-#p + geom_hline(aes(yintercept=160000)) + geom_text(aes(0,160000,label = 'Тест', vjust = -0.5, hjust=-0.5), inherit.aes=FALSE)
+if (jitter) {
+    geom_ <- c("jitter", "boxplot"); 
+} else {
+    geom_ <- c("boxplot"); 
+}
 
-qplot(lang, salary, data=combined, geom="boxplot", alpha=I(1), main="Сравнение предложений на рынке труда для разработчиков", 
-      xlab="", ylab="Заработная плата ('net')", varwidth = TRUE, colour=area.name)
-      
+if (jitter) {
+    # Отключаем изображение выбросов, чтобы они не дублировали точки вакансий
+    outlier.colour_ <- NA
+} else {
+    outlier.colour_ <- I("blue")
+}
+
+alpha_ <- I(0.8)
+ylab_ <- 'Заработная плата (нетто)'
+
+if (is.na(area_list)) {
+    if (expcolour) {
+        plot <- qplot(x="", y=salary, data=vacancies, 
+            facets= . ~ search, geom=geom_, alpha=alpha_, colour=experience,
+            outlier.colour=outlier.colour_, main=title, xlab="", ylab=ylab_, varwidth = boxwidth)
+    } else {
+        plot <- qplot(x="", y=salary, data=vacancies,
+            facets= . ~ search, geom=geom_, alpha=alpha_,
+            outlier.colour=outlier.colour_, main=title, xlab="", ylab=ylab_, varwidth = boxwidth)
+    }
+} else {
+    if (expcolour) {
+        plot <- qplot(x=area.name, y=salary, data=vacancies, 
+            facets= . ~ search, geom=geom_, alpha=alpha_, colour=experience,
+            outlier.colour=outlier.colour_, main=title, xlab="", ylab=ylab_, varwidth = boxwidth)
+    } else {
+        plot <- qplot(x=area.name, y=salary, data=vacancies,
+            facets= . ~ search, geom=geom_, alpha=alpha_,
+            outlier.colour=outlier.colour_, main=title, xlab="", ylab=ylab_, varwidth = boxwidth)
+    }
+}
+
+print(plot)
+dev.off()
+
+requestDir <- '/mnt/webapp-files/${resourceName}/${requestId}'
+dir.create(requestDir, recursive=TRUE)
+
+chartFile <- paste(requestDir, 'chart.png', sep='/')
+CairoPNG(chartFile, width=plotwidth, height=plotheight)
+print(plot)
+dev.off()
+
+vacancies
