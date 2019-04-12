bd170fabffcf65773e1400958ee654af9d16cc41
Kirill <kamikhailov@gmail.com>
2019-04-12 18:56:20 +0300

    Анализ рынка труда по данным HeadHunter

--- hh.R
+++ hh.R
@@ -48,7 +48,7 @@ getData <- function(url) {
     return(out)
 }
 
-getVacancies <- function(exp, search) {
+getVacancies <- function(exp, search, area.id) {
     result <- NULL
 
     p <- 0
@@ -63,11 +63,6 @@ getVacancies <- function(exp, search) {
             params_list <- append(params_list, specialization_list)
         }
         
-        if (!is.na(area_list)) {
-            names(area_list) <- rep(c("area"), length(area_list))
-            params_list <- append(params_list, area_list)
-        }
-        
         if (!is.na(industry_list)) {
             names(industry_list) <- rep(c("industry"), length(industry_list))
             params_list <- append(params_list, industry_list)
@@ -78,12 +73,19 @@ getVacancies <- function(exp, search) {
             params_list <- append(params_list, schedule_list)
         }
         
+        if (!is.na(employment_list)) {
+            names(employment_list) <- rep(c("employment"), length(employment_list))
+            params_list <- append(params_list, employment_list)
+        }
+        
         # Параметры, имеющие несколько значений для одних и тех же имён, надо установливать первыми
+        #TODO Может ли params_list быть пустым?
         url <- modify_url(url, query = params_list)
         
         url <- modify_url(url, query = list(page=p, per_page=100))
-        url <- modify_url(url, query = list(only_with_salary="true", no_magic="false", search_field="name"))
+        url <- modify_url(url, query = list(only_with_salary="true", no_magic="true", search_field="name"))
         url <- modify_url(url, query = list(text=search))
+        url <- modify_url(url, query = list(area=area.id))
         
         if (!is.null(exp)) {
             url <- modify_url(url, query = list(experience=exp))
@@ -118,7 +120,7 @@ getVacancies <- function(exp, search) {
   return(result)
 }
 
-log("========== Начало выполнения ==========")
+log("Начало выполнения")
 
 # Проверка и обработка параметров
 
@@ -141,16 +143,52 @@ if (schedule_str == 'NA') {
     }
 }
 
-# Регион
-area_str <- '${area}'
-if (area_str == 'NA') {
-    area_list <- NA
+# Тип занятости
+employment_str <- '${employment}'
+if (employment_str == 'NA') {
+    employment_list <- NA
 } else {
-    if (!grepl("^\\d+(,\\d+)*$", area_str)) {
-        stop(paste("Недопустимое значение параметра региона:", area_str, sep=" "));
+    if (!grepl("^(full|part|project|volunteer|probation)(,(full|part|project|volunteer|probation))*$", employment_str)) {
+        stop(paste("Недопустимое значение параметра типа занятости:", employment_str, sep=" "))        
     } else {
-        area_list <- as.list(strsplit(area_str, ",")[[1]])    
+        employment_list <- as.list(strsplit(employment_str, ",")[[1]]) 
     }
+}
+
+# Идентификаторы регионов
+areaids_str <- '${areaids}'
+if (areaids_str == 'NA') {
+    stop("Не установлено значение параметра идентификаторов регионов")
+} else {
+    if (!grepl("^\\d+(,\\d+)*$", areaids_str)) {
+        stop(paste("Недопустимое значение параметра индентификаторов регионов:", areaids_str, sep=" "));
+    } else {
+        areaids_list <- as.list(strsplit(areaids_str, ",")[[1]])    
+    }
+}
+
+# Названия регионов
+areanames_str <- '${areanames}'
+if (areanames_str == 'NA') {
+    stop("Не установлено значение параметра названий регионов")
+} else {
+    if (!grepl("^[ .А-Яа-я0-9()-]+(,[ .А-Яа-я0-9()-]+)*$", areanames_str)) {
+        stop(paste("Недопустимое значение параметра названий регионов:", areanames_str, sep=" "));
+    } else {
+        areanames_list <- as.list(strsplit(areanames_str, ",")[[1]])    
+    }
+}
+
+if (length(areaids_list) != length(areanames_list)) {
+    stop("Количество идентификаторов не соответствует количеству названий регионов")
+}
+
+areas_list <- setNames(areaids_list, areanames_list)
+
+# Объединить все регионы
+groupareas <- as.logical('${groupareas}')
+if (is.na(groupareas)) {
+    groupareas <- FALSE
 }
 
 # Профобласть или специализация
@@ -159,6 +197,7 @@ if (specialization_str == 'NA') {
     specialization_list <- NA
 } else {
     if (!grepl("^\\d+(\\.\\d+)?(,\\d+(\\.\\d+)?)*$", specialization_str)) {
+        # TODO Логирование ошибки ?
         stop(paste("Недопустимое значение параметра специализации:", specialization_str, sep=" "));
     } else {
         specialization_list <- as.list(strsplit(specialization_str, ",")[[1]])    
@@ -192,7 +231,7 @@ if (is.na(jitter)) {
 # Параметры границ
 limitmin_str <- '${limitmin}'
 if (limitmin_str == "NA") {
-    limitmin <- NA
+    limitmin <- FALSE
 } else {
     limitmin <- as.numeric(limitmin_str)
     if (is.na(limitmin)) {
@@ -201,7 +240,7 @@ if (limitmin_str == "NA") {
 }
 limitmax_str <- '${limitmax}'
 if (limitmax_str == "NA") {
-    limitmax <- NA
+    limitmax <- FALSE
 } else {
     limitmax <- as.numeric(limitmax_str)
     if (is.na(limitmax)) {
@@ -224,38 +263,66 @@ if (is.na(plotheight)) {
     stop(paste("Недопустимое значение параметра высоты диаграммы:", plotheight_str, sep=" "))
 }
 
-# Ширина ящиков зависит от количества вакансий в соответствующей группе
+# Должна ли ширина ящиков зависеть от количества вакансий в соответствующей группе
 boxwidth <- as.logical('${boxwidth}')
 if (is.na(jitter)) {
     boxwidth <- FALSE
 }
 
+# Горизонтальные линии (отметки)
+hlines_str <- '${hlines}'
+if (hlines_str == 'NA') {
+    hlines <- integer()
+} else {
+    if (!grepl("^\\d+(,\\d+)*$", hlines_str)) {
+        stop(paste("Недопустимое значение параметра отметок:", hlines_str, sep=" "));
+    } else {
+        hlines <- as.integer(strsplit(hlines_str, ",")[[1]])    
+    }
+}
+
+# Шаг делений вертикальной оси
+breaksby_str <- '${breaksby}'
+breaksby <- as.numeric(breaksby_str)
+if (is.na(breaksby)) {
+    stop(paste("Недопустимое значение параметра шага делений:", breaksby_str, sep=" "))
+}
+
 vacancies <- NULL
 
-{ # XXX Цикл, не вложенный в фигурные скобки, приводит к ошибке RServe
-for (search in search_list) {
-    if (expcolour) {
-        experience <- c("noExperience", "between1And3", "between3And6", "moreThan6")    
-        for (exp in experience) {
-            result <- getVacancies(exp, search) 
-            if (is.null(vacancies)) {
-                vacancies <- result
-            } else {
-                vacancies <- rbind(result, vacancies)
+{ # XXX
+for (name in names(areas_list)) {
+    for (search in search_list) {
+        if (expcolour) {
+            experience <- c("noExperience", "between1And3", "between3And6", "moreThan6")    
+            for (exp in experience) {
+                result <- getVacancies(exp, search, areas_list[[name]]) 
+                if (!is.null(result)) {
+                    result$area.search_name <- factor(name)
+                    if (is.null(vacancies)) {
+                        vacancies <- result
+                    } else {
+                        vacancies <- rbind(vacancies, result)
+                    }
+                }
             }
-        } 
-    } else {
-        result <- getVacancies(NULL, search) 
-        if (is.null(vacancies)) {
-            vacancies <- result
         } else {
-            vacancies <- rbind(result, vacancies)
+            result <- getVacancies(NULL, search, areas_list[[name]]) 
+            if (!is.null(result)) {
+                result$area.search_name <- factor(name)
+                if (is.null(vacancies)) {
+                    vacancies <- result
+                } else {
+                    vacancies <- rbind(vacancies, result)
+                }
+            }
         }
     }
 }
 } # XXX
 
 if (length(vacancies) == 0) {
+    # TODO Логировать количество вакансий
     stop("Отсутствуют элементы для отображения")
 }
 
@@ -272,17 +339,17 @@ options(scipen=999)
 
 CairoFonts(regular='DejaVu Serif:style=Regular', bold='DejaVu Serif:style=Bold')
 
-if (!is.na(area_list)) {
+if (!groupareas) {
     if (expcolour) {
-        aes <- aes(x=area.name, y=salary, colour=experience)
+        aes <- aes(x=area.search_name, y=salary, colour=experience)
     } else {
-        aes <- aes(x=area.name, y=salary)
+        aes <- aes(x=area.search_name, y=salary)
     }
 } else {
     if (expcolour) {
-        aes <- aes(x="Все регионы", y=salary, colour=experience)
+        aes <- aes(x=areanames_str, y=salary, colour=experience)
     } else {
-        aes <- aes(x="Все регионы", y=salary)
+        aes <- aes(x=areanames_str, y=salary)
     }
 }
 
@@ -293,17 +360,22 @@ if (jitter) {
     # Отключаем изображение выбросов, чтобы они не дублировали точки вакансий
     plot <- plot + geom_boxplot(varwidth = boxwidth, outlier.colour = NA, alpha = 0.8)
 } else {
-    plot <- plot + geom_boxplot(varwidth = boxwidth, outlier.colour = I("red"), alpha = 0.8)
+    plot <- plot + geom_boxplot(varwidth = boxwidth, outlier.colour = I("gray"), alpha = 0.8)
 }
 
 plot <- plot + facet_grid(. ~ search) + labs(title=maintitle, x="", y="Заработная плата (нетто)", color="Опыт работы") +
     theme(axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 10), legend.text=element_text(size = 10),
         legend.title=element_text(size = 10, face="bold"), strip.text.x=element_text(size = 10, face="bold"))
-            
-if (!is.na(limitmin) || !is.na(limitmax)) {
-    plot <- plot + ylim(limitmin, limitmax)
+    
+if (limitmin || limitmax) {
+    plot <- plot + coord_cartesian(ylim = c(limitmin, limitmax))    
 }
 
+# Выводим отметки и задаём шаг делений вертикальной оси
+plot <- plot + geom_hline(yintercept=hlines, color="gray", linetype=5) + 
+            scale_y_continuous(breaks=sort(c(seq(0,max(vacancies$salary, na.rm = TRUE),by=breaksby), hlines)))
+
+# TODO Закомментировать
 print(plot)
 dev.off()
 
@@ -316,3 +388,4 @@ print(plot)
 dev.off()
 
 vacancies
+
